9bf6e3e040d6e8640a5fc08efcfcdf49828f6e83
Piotr Osiak <piotr.osiak@hotmail.co.uk>
2016-03-10 22:49:35 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript Project
+
+Practical Self-Initiated project based on the JavaScript.
+
+## Purpose
+
+This is a self-initiated project which is a part of an assignment for University.
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	minifyHTML = require('gulp-minify-html'),
+	concat = require('gulp-concat');
+	
+
+var env, 
+	jsSources,
+	sassSources,
+	htmlSources,
+	outputDir,
+	sassStyle;
+
+env = 'development';
+
+if (env==='development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+} 
+
+jsSources = ['components/scripts/script.js'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(browserify())
+	.pipe(gulpif(env === 'production', uglify()))
+	.pipe(gulp.dest(outputDir + 'js'))
+	.pipe(connect.reload())
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+	.pipe(compass({
+		sass: 'components/sass',
+		css: outputDir + 'css',
+		image: outputDir + 'images',
+		style: sassStyle
+	}))
+	.on('error', gutil.log)
+	.pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+	gulp.watch(jsSources, ['js']);
+	gulp.watch(['components/sass/*.scss', 'components/sass/*/*.scss'], ['compass']);
+	gulp.watch('builds/development/*.html', ['html']);
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		root: outputDir,
+		livereload: true
+	});
+});
+
+gulp.task('html', function() {
+	gulp.src('builds/development/*.html')
+	.pipe(gulpif(env === 'production', minifyHTML()))
+	.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+	.pipe(connect.reload())
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'compass', 'connect']);
